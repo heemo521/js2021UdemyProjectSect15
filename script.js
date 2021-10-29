f4a39813875b9ea017f10ae271af7468d9ff253c
heemo521 <heemo521@gmail.com>
2021-10-29 00:43:00 -0700

    pre-structure

--- script.js
+++ script.js
@@ -11,28 +11,78 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
+      //   console.log(position);
+
+      //current location lat, lng
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},@${longitude}`);
-
+      //   console.log(`https://www.google.com/maps/@${latitude},@${longitude}`);
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      map = L.map('map').setView(coords, 13); //second argument is zoom level
+      //   console.log(map);
+
+      //maps are made of small tiles sourced from the openstreemap here - style fr/hot/
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      //creating marker
+      //   L.marker(coords)
+      //     .addTo(map)
+      //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      //     .openPopup();
+
+      //Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE; //mapEvent declared globally then assigned here with this eventHandler to be used elsewhere
+        form.classList.remove('hidden'); //reveal form when clicked on map
+        inputDistance.focus(); //immedediately move the cursor to the form box
+      });
     },
     function () {
       alert('Could not get your position');
     }
   );
+
+//Form submit
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  //L.marker([location]) .addTo the (map) .bindPopup(message to show)
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        //find customization options from documentation of Leaflet
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function (e) {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
